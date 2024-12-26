12d93e21b0e10883b71a13fd8c758e9f7f70a4b1
Nadib Rana <codecrafersnadib@gmail.com>
2024-12-26 12:08:43 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+// ================For navigation=============
+
+const hamburger = document.getElementById('nav-portfolio-hamburger');
+const navLinks = document.getElementById('nav-portfolio-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const images = document.querySelectorAll(".social-img"); // Select all images
+  let currentIndex = 0;
+
+  const showImagesSequentially = () => {
+    if (currentIndex < images.length) {
+      // Add "visible" class to the next image
+      images[currentIndex].classList.add("visible");
+      currentIndex++;
+
+      // Continue showing the next image after 1 second
+      setTimeout(showImagesSequentially, 1000);
+    }
+  };
+
+  // Start the sequence
+  showImagesSequentially();
+});
